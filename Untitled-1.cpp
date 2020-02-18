0a211da30ba9a6ec1f69a4834ce765ce667a9469
theglitchh <higgsbosson10@gmail.com>
2020-02-18 22:03:37 +0530

    codes: some simple c++ codes

---
+++ Untitled-1.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main ()
+{ x = 1; 
+  y = 2;
+   
---
+++ ifcont.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int getMax( int num1, int num2, int num3){
+int result;
+
+
+if (num1 <= num2 && num1 <= num3) {
+
+result = num1;
+} else if ( num2 <= num1 && num2 <= num3) {
+
+    result = num2;
+} else {
+
+    result = num3;
+}
+return result;
+}
+
+int main ()
+{ 
+int num1;
+int num2;
+int num3;
+cin >> num1>>num2>>num3;
+cout << getMax(num1,num2,num3);
+return 0;}
